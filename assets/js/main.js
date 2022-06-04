7c2518819f015f759a39f57e492e7197167bc2c1
WhatAPhantasia <81176405+WhatAPhantasia@users.noreply.github.com>
2022-06-04 14:07:14 -0500

    bg fix and temp toplight stick

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,15 +13,35 @@ window.onclick = function(event) {
   }
 }
 
-/* Set the width of the sidebar to 250px */
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("toplights").style.top = "0";
+  } else {
+    document.getElementById("toplights").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+/*window.addEventListener("scroll",function() { 
+   if(window.scrollY > 10) {
+      $('.toplights').slideUp();
+   }
+   else {
+      $('.toplights').slideDown();
+   }
+},false);*/
+
+/* Set the width of the sidebar to 250px 
 function openNav() {
   document.getElementById("mySidepanel").style.width = "250px";
 }
 
-/* Set the width of the sidebar to 0. i guess i dont need this anymore but keeping it just in case */
+// Set the width of the sidebar to 0. i guess i dont need this anymore but keeping it just in case 
 function closeNav() {
   document.getElementById("mySidepanel").style.width = "0";
-}
+}*/
 
 function toggleSidebar() {
 	if (document.getElementById("mySidepanel").style.width="0px") {
