570e6c25b41e70d2bd97339a178f2c80c7fb58ac
WhatAPhantasia <81176405+WhatAPhantasia@users.noreply.github.com>
2023-05-29 23:53:46 -0500

    march 2023 update!
    finally fricking changed to bootstrap, and did most of the visuals for the homepageee. just gotta play with it some more yk

---
+++ assets/js/bs_main.js
@@ -0,0 +1,9 @@
+//toplight scroll thing? dunno how to set this up w bootstrap
+window.addEventListener("scroll",function() { 
+    if(window.scrollY > 200) {
+       document.getElementById("toplights").style.top = "-10px";
+    }
+    else {
+       document.getElementById("toplights").style.top = "50px";
+    }
+ },false);
