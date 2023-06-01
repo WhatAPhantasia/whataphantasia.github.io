04bf635254b21dee534bec7dad9d0353fa91d206
WhatAPhantasia <81176405+WhatAPhantasia@users.noreply.github.com>
2023-06-01 08:52:16 -0500

    aboutme + try to fix toplights

--- assets/js/bs_main.js
+++ assets/js/bs_main.js
@@ -1,9 +1,12 @@
 //toplight scroll thing? dunno how to set this up w bootstrap
+
 window.addEventListener("scroll",function() { 
-    if(window.scrollY > 200) {
-       document.getElementById("toplights").style.top = "-10px";
-    }
-    else {
-       document.getElementById("toplights").style.top = "45px";
-    }
+   var vtop = window.getComputedStyle(document.querySelector('#toplights')).top
+   if(window.scrollY > 200) {
+      document.getElementById("toplights").style.top = "-10px";
+   }
+   else {
+      /*document.getElementById("toplights").style.top = vtop;*/
+      document.getElementById("toplights").style.top = "45px";
+   }
  },false);
